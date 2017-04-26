5da0c74c257eedd0cf2e412d463f292030ba836e
James Armstrong <intellemerc@James-Macbook.lan>
2017-04-25 21:08:11 -0400

    Typos, rewording, cleanup

--- presentation/index.js
+++ presentation/index.js
@@ -58,18 +58,31 @@ export default class Presentation extends React.Component {
         <Slide transition={["fade"]} bgColor="tertiary">
           <Heading size={6} textColor="primary" caps>Why Typescript</Heading>
           <List textColor="primary">
-            <Appear><ListItem>Superset of Javascript</ListItem></Appear>
+            <Appear><ListItem>Superset of Javascript(Commitment to EcmaScript compatibility)</ListItem></Appear>
             <Appear><ListItem>Can Mix JS and TS</ListItem></Appear>
             <Appear><ListItem>Zero runtime cost <span style={{ fontSize: 14 }}>(ts disappears at runtime)</span></ListItem></Appear>
-            <Appear><ListItem>Great intellisense/autocomplete support</ListItem></Appear>
+            <Appear><ListItem>Great tooling(Intellisense, autocomplete, refactor, errors, etc)</ListItem></Appear>
             <Appear><ListItem>Type inference</ListItem></Appear>
             <Appear><ListItem>Mature ecosystem</ListItem></Appear>
+            <Appear><ListItem>Kept up to date(bi-monthly releases)</ListItem></Appear>
           </List>
         </Slide>
         <Slide transition={["fade"]} bgColor="primary" textColor="tertiary">
           <Heading size={6} textColor="secondary" caps>Typescript - Usage(NPM downloads)</Heading>
           <Image src={images.npmDownloads} />
           <Link href="https://npm-stat.com/charts.html?package=babel&package=typescript&package=flow-bin&from=2016-10-01&to=2017-03-31" target="blank" >Source</Link>
+        </Slide>
+        <Slide transition={["fade"]} bgColor="tertiary">
+          <Heading size={6} textColor="primary" caps>Projects using Typescript</Heading>
+          <List textColor="primary">
+            <Appear><ListItem>Angular</ListItem></Appear>
+            <Appear><ListItem>VSCode</ListItem></Appear>
+            <Appear><ListItem>Slack</ListItem></Appear>
+            <Appear><ListItem>Glimmer(Ember rendering engine)</ListItem></Appear>
+            <Appear><ListItem>Apollo GraphQL</ListItem></Appear>
+            <Appear><ListItem>RxJS</ListItem></Appear>
+            <Appear><ListItem>CycleJs</ListItem></Appear>
+          </List>
         </Slide>
         <Slide transition={["fade"]} bgColor="tertiary">
             <Heading size={6} textColor="primary" caps>How does this help me?</Heading>
@@ -81,15 +94,15 @@ export default class Presentation extends React.Component {
           code={require("raw-loader!../code/createTypescript.example")}
           ranges={[
               { loc: [0, 8], title: "Easy Way - Greenfield" },
-              { loc: [2, 3], title: "Install CRA" },
-              { loc: [4, 5], title: "Run CRA with Scripts" },
-              { loc: [5, 7], title: "Start the project" }
+              { loc: [0, 1], title: "Install CRA" },
+              { loc: [2, 3], title: "Run CRA with Scripts", note: "Uses template/script to create project" },
+              { loc: [3, 5], title: "Start the project" }
           ]}
         />
         <Slide transition={["fade"]} bgColor="tertiary">
           <Heading size={6} textColor="primary" caps>Existing project: Init</Heading>
           <List textColor="primary">
-            <Appear><ListItem textSize={24}>Use CRA/Typescript as template(eject)</ListItem></Appear>
+            <Appear><ListItem textSize={24}>Use CRA/Typescript as reference(eject)</ListItem></Appear>
             <Appear><ListItem textSize={24}>Yarn add --dev typescript ts-loader tslint tslint-loader tslint-react @types/jest @types/node @types/react @types/react-dom</ListItem></Appear>
           </List>
         </Slide>
@@ -97,7 +110,7 @@ export default class Presentation extends React.Component {
           <Heading size={6} textColor="secondary" caps>Existing Project: Webpack</Heading>
           <List>
             <Appear><ListItem>Add entry Resolve>Extensions</ListItem></Appear>
-            <Appear><ListItem>Add entry for ts-lint</ListItem></Appear>
+            <Appear><ListItem>Add entry for Preloaders>ts-lint</ListItem></Appear>
             <Appear><ListItem>Add entry to exlude ts(x) from url loader </ListItem></Appear>
             <Appear><ListItem>Add entry for ts(x)files passing to ts-loader</ListItem></Appear>
           </List>
@@ -105,24 +118,20 @@ export default class Presentation extends React.Component {
         <Slide transition={["fade"]} bgColor="tertiary">
             <Heading size={6} textColor="primary" caps>Existing Project: Jest support</Heading>
             <List>
-              <Appear><ListItem textColor="primary">Add support for Jest</ListItem></Appear>
+              <Appear><ListItem textColor="primary">Add support for Jest in project.json</ListItem></Appear>
               <Appear><ListItem textColor="primary">Add TS Transform for Jest</ListItem></Appear>
             </List>
-        </Slide>
-        <Slide transition={["fade"]} bgColor="tertiary">
-            <Heading size={6} textColor="primary" caps>Existing Project: Webpack</Heading>
-            <Text textAlign="Center" lineHeight={5} textSize={52} bold caps>Demo</Text>
         </Slide>
         <Slide transition={["fade"]} bgColor="primary" textColor="tertiary">
           <Heading size={5} textColor="secondary" caps>Existing Project: Hardest part</Heading>
           <List>
             <Appear><ListItem textColor="tertiary" lineHeight={5} >Rename js files to ts/tsx</ListItem></Appear>
-            <Appear><ListItem textColor="tertiary" lineHeight={5} >Fix components to use generics</ListItem></Appear>
+            <Appear><ListItem textColor="tertiary" lineHeight={5} >Fix components to use generic declaration</ListItem></Appear>
           </List>
         </Slide>
         <Slide transition={["fade"]} bgColor="tertiary">
-          <Heading size={4} textColor="primary" caps>Using Typescript in React</Heading>
-          <Heading Size={6}>Demo</Heading>
+            <Heading size={6} textColor="primary" caps>Existing Project: Webpack and Using Typescript</Heading>
+            <Text textAlign="Center" lineHeight={5} textSize={52} bold caps>Demo</Text>
         </Slide>
       </Deck>
     );
